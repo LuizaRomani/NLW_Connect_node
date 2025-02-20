96a9fde76b366064acb1a8d7c42757317af29f57
Luiza Sofal <lrsofal@sga.pucminas.br>
2025-02-20 18:44:49 -0300

    functions

---
+++ src/functions/access-invite-link.ts
@@ -0,0 +1,10 @@
+import { redis } from '../redis/client'
+
+interface AccessInviteLinkParams {
+  subscriberId: string
+}
+
+export async function accessInviteLink({ subscriberId }: AccessInviteLinkParams) {
+  await redis.hincrby('referral:access-count', subscriberId, 1)
+  // adicionanr o id do usuario e ir incrementando a cada chamada. 'hash increment by'
+}
